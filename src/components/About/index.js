70574e87df582b43c0d69f56034f1a49c9a28eac
tj-tieso <tj.tieso@gmail.com>
2022-08-01 16:44:20 -0500

    about rendering

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">About</h1>
+      <p>
+        Trevor Tieso is Full-Stack Web Developer with a background in Customer
+        Service and Management. A graduate of the University of Kansas, with a
+        certificate in Full Stack Development. Ready to use newly honed skills
+        in JavaScript, CSS, React.js, and responsive web design. Familiar with
+        the MERN stack, OOP, and TDD. A passionate learner, with a customer
+        first mindset, ready to contribute to your team!
+      </p>
+      <p>
+        Based in Kansas City, USA. Trevor enjoys spending time with his wife,
+        Megan, and his two rescue dogs, Mac and Minnie. Passions include, but
+        are not limited too: Dungeons and Dragons, Coffee, Soccer, and fine
+        spirits.
+      </p>
+    </section>
+  );
+}
+
+export default About;
