7eba166a6d957c1c520517c18775b9b6f542edde
tj-tieso <tj.tieso@gmail.com>
2022-08-03 16:50:21 -0500

    images rendering

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,8 +6,8 @@ function Portfolio() {
     {
       name: "SmartBook",
       link: "https://smartest-book.herokuapp.com/",
-      github: "https://github.com/kylebreynolds/smart-book",
-      image: "",
+      github: "",
+      image: "smart-book",
       description:
         "Team Project Built with JS, Handlebars, Express, and my MySql",
     },
@@ -15,37 +15,37 @@ function Portfolio() {
       name: "Campaign Blog",
       link: "https://blogged-down.herokuapp.com/",
       github: "https://github.com/tj-tieso/blogged-down",
-      image: "",
+      image: "blogged-down",
       description: "A blog for you Table Top Roll Playing game.",
     },
-    {
-      name: "No Budge Budget",
-      link: "https://no-budge-budget.herokuapp.com/",
-      github: "https://github.com/tj-tieso/no-budge-budget",
-      image: "",
-      description: "An existing Mock Budget App, converted into a PWA",
-    },
-    {
-      name: "Team Profile Generator",
-      link: "https://github.com/tj-tieso/team-profile-gen",
-      github: "https://github.com/tj-tieso/team-profile-gen",
-      image: "",
-      description: "A CLI to generate contact cards for a team",
-    },
-    {
-      name: "Project 5",
-      link: "https://github.com/tj-tieso",
-      github: "https://github.com/tj-tieso",
-      image: "",
-      description: "",
-    },
-    {
-      name: "Project 6",
-      link: "https://github.com/tj-tieso",
-      github: "https://github.com/tj-tieso",
-      image: "",
-      description: "",
-    },
+    // {
+    //   name: "No Budge Budget",
+    //   link: "https://no-budge-budget.herokuapp.com/",
+    //   github: "https://github.com/tj-tieso/no-budge-budget",
+    //   image: "budget",
+    //   description: "An existing Mock Budget App, converted into a PWA",
+    // },
+    // {
+    //   name: "Team Profile Generator",
+    //   link: "https://github.com/tj-tieso/team-profile-gen",
+    //   github: "https://github.com/tj-tieso/team-profile-gen",
+    //   image: "team-profile",
+    //   description: "A CLI to generate contact cards for a team",
+    // },
+    // {
+    //   name: "Project 5",
+    //   link: "https://github.com/tj-tieso",
+    //   github: "https://github.com/tj-tieso",
+    //   image: "project5",
+    //   description: "",
+    // },
+    // {
+    //   name: "Project 6",
+    //   link: "https://github.com/tj-tieso",
+    //   github: "https://github.com/tj-tieso",
+    //   image: "project5",
+    //   description: "",
+    // },
   ];
   return (
     <section className="portfolioBackdrop">
@@ -54,10 +54,10 @@ function Portfolio() {
           <img
             className="projectBackgroundImg"
             alt="project screenshot"
-            src={project.image}
+            src={require(`../../assets/projects/${project.image}.png`)}
           />
           <h3 className="projectTitle">{project.name}</h3>
-          <a href={project.github}>
+          <a href={project.github} target="_blank" rel="noreferrer">
             <img alt="github icon" src={GitHubMark} />
           </a>
           <p>{project.description}</p>
