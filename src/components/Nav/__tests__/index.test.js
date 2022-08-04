700424b0618afabd310f49440843c145d530de1f
tj-tieso <tj.tieso@gmail.com>
2022-08-04 09:37:45 -0500

    links test

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -5,7 +5,7 @@ import Nav from "..";
 
 afterEach(cleanup);
 
-describe("Nav Component", () => {
+describe("Nav Component : ", () => {
   it("renders", () => {
     render(<Nav />);
   });
@@ -14,5 +14,15 @@ describe("Nav Component", () => {
     // asFragment functionreturns a snapshot of the tested component
     const { asFragment } = render(<Nav />);
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe("Links are Visible", () => {
+  it("inserts text into links", () => {
+    const { getByTestId } = render(<Nav />);
+    expect(getByTestId("link")).toHaveTextContent("Tieso");
+    expect(getByTestId("about")).toHaveTextContent("About");
+    expect(getByTestId("portfolio")).toHaveTextContent("Portfolio");
+    expect(getByTestId("contact")).toHaveTextContent("Contact");
   });
 });
